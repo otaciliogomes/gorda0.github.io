e2a401dbe50cf554988aaaa9ef42f650e523ccb5
Douglas Guimarães <douglasguimaraes@ext.cvccorp.com.br>
2021-12-04 18:59:29 -0300

    create utils

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+export const paginate = (items, perPage) =>
+    items.reduce(
+        (acc, _, index) => {
+            if (index % perPage === 0)
+                acc.push(items.slice(index, index + perPage))
+
+            return acc
+        },
+        []
+    )
+
+export const keysMiddleware = async (e, fn) => {
+    await e.preventDefault()
+    await e.stopPropagation()
+    return fn()
+}
